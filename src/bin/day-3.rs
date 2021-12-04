9a407d6739460d79207ca21a93d70e4a269d6c3b
denis114 <denis@meshcapade.com>
2021-12-04 23:28:46 +0100

    Dont look, this happened at 12 at night

--- src/bin/day-3.rs
+++ src/bin/day-3.rs
@@ -4,11 +4,12 @@ use std::fmt;
 use std::fs;
 
 const INPUT: &str = "src/inputs/day-3.txt";
-
+const BIT_COUNT: usize = 12;
 fn read_input(input: &str) -> String {
     fs::read_to_string(input).expect("Unable to open input")
 }
 
+#[derive(Debug, Clone)]
 struct Diagnostic<const BIT: usize> {
     accum: Vec<i32>,
 }
@@ -79,13 +80,50 @@ impl<const BIT: usize> Diagnostic<BIT> {
     }
 }
 
+fn most_common_value_at_pos(vec: &Vec<Diagnostic<BIT_COUNT>>, pos: i32, default_at_0: i32) -> i32 {
+    let result = vec.iter().fold(0, |accum, x| {
+        let value = x.accum[pos as usize];
+        match value {
+            0 => accum - 1,
+            1 => accum + 1,
+            _ => accum,
+        }
+    });
+    if result == 0 {
+        return default_at_0;
+    }
+    (result > 0) as i32
+}
+
+
+fn least_common_value_at_pos(vec: &Vec<Diagnostic<BIT_COUNT>>, pos: i32, default_at_0: i32) -> i32 {
+    let result = vec.iter().fold(0, |accum, x| {
+        let value = x.accum[pos as usize];
+        match value {
+            0 => accum - 1,
+            1 => accum + 1,
+            _ => accum,
+        }
+    });
+    if result == 0 {
+        return default_at_0;
+    }
+    (result < 0) as i32
+}
+
 fn calculate_power_consumption(input_path: &str) -> i32 {
     let str_data = read_input(input_path);
 
-    let accum_diagnostic: Diagnostic<12> = str_data
+    let accum_diagnostic: Diagnostic<BIT_COUNT> = str_data
         .split('\n')
-        .map(|str_data| Diagnostic::<12>::values_from_str(str_data).ok().unwrap())
-        .fold(Diagnostic::<12>::default(), |acc, diag| acc.add(diag));
+        .map(|str_data| {
+            Diagnostic::<BIT_COUNT>::values_from_str(str_data)
+                .ok()
+                .unwrap()
+        })
+        .fold(Diagnostic::<BIT_COUNT>::default(), |acc, diag| {
+            acc.add(diag)
+        });
 
     let gamma = accum_diagnostic.gamma_rate();
     let epsilon = accum_diagnostic.epsilon_rate();
@@ -93,17 +131,48 @@ fn calculate_power_consumption(input_path: &str) -> i32 {
     gamma * epsilon
 }
 
+fn calculate_oxygen_rating(diagnostics: &Vec<Diagnostic<BIT_COUNT>>) -> i32 {
+    let mut d = diagnostics.clone();
+    let mut bit_flag_position = 0;
+    while d.len() != 1 {
+        let most_common_bit = most_common_value_at_pos(&d, bit_flag_position, 1);
+        d.retain(|x| x.accum[bit_flag_position as usize] == most_common_bit );
+        bit_flag_position += 1;
+    }
+    let mvp_diagnostics = d.first().unwrap().accum.clone();
+    println!("oxygen: {:?}",mvp_diagnostics);
+    Diagnostic::<BIT_COUNT>::vec_to_integer(mvp_diagnostics)
+}
+
+fn calculate_co2_rating(diagnostics: &Vec<Diagnostic<BIT_COUNT>>) -> i32 {
+    let mut d = diagnostics.clone();
+    let mut bit_flag_position = 0;
+    while d.len() != 1 {
+        let most_common_bit = least_common_value_at_pos(&d, bit_flag_position, 0);
+        d.retain(|x| x.accum[bit_flag_position as usize] == most_common_bit );
+        bit_flag_position += 1;
+    }
+    let mvp_diagnostics = d.first().unwrap().accum.clone();
+    println!("co2: {:?}",mvp_diagnostics);
+    Diagnostic::<BIT_COUNT>::vec_to_integer(mvp_diagnostics)
+}
+
 fn calculate_life_support(input_path: &str) -> i32 {
     let str_data = read_input(input_path);
     // Parse input
-    let diagnostics: Vec<Diagnostic<12>> = str_data
+    let diagnostics: Vec<Diagnostic<BIT_COUNT>> = str_data
         .split('\n')
         .map(|str_data| {
-            Diagnostic::<12>::new(Diagnostic::<12>::values_from_str(str_data).ok().unwrap())
+            Diagnostic::<BIT_COUNT>::new(
+                Diagnostic::<BIT_COUNT>::values_from_str(str_data)
+                    .ok()
+                    .unwrap(),
+            )
         })
         .collect();
-
-    0
+    let oxygen_rating = calculate_oxygen_rating(&diagnostics);
+    let co2_rating = calculate_co2_rating(&diagnostics);
+    oxygen_rating * co2_rating
 }
 
 fn main() {
