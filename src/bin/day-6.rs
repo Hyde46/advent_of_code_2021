9ce94f54ae9f8aacaabaeb2de21f0004cbd15f9b
Denis <denis.heid@protonmail.com>
2021-12-07 11:24:02 +0100

    Finish day 7

--- src/bin/day-6.rs
+++ src/bin/day-6.rs
@@ -16,11 +16,16 @@ fn simulate_cached_fish(input: &str, days: usize) -> usize {
     // 9 possible fish states
     let mut fish_cache: Vec<usize> = vec![0; 9];
     let mut fish_swap: Vec<usize> = vec![0; 9];
-    fish.iter().for_each(|f| fish_cache[*f as usize] = fish_cache[*f as usize] + 1);
+    fish.iter()
+        .for_each(|f| fish_cache[*f as usize] = fish_cache[*f as usize] + 1);
     for _i in 1..days + 1 {
         fish_cache.iter().enumerate().for_each(|(i, f)| {
-            let target_swap_index = if i == 0 {6} else {i - 1};
-            fish_swap[target_swap_index] = if fish_swap[target_swap_index] == 0 {*f} else {*f + fish_swap[target_swap_index]};
+            let target_swap_index = if i == 0 { 6 } else { i - 1 };
+            fish_swap[target_swap_index] = if fish_swap[target_swap_index] == 0 {
+                *f
+            } else {
+                *f + fish_swap[target_swap_index]
+            };
             if i == 0 {
                 fish_swap[8] = *f;
             }
@@ -33,5 +38,8 @@ fn simulate_cached_fish(input: &str, days: usize) -> usize {
 }
 
 fn main() {
-    println!("Cached fish simulation {}", simulate_cached_fish(INPUT, 256));
+    println!(
+        "Cached fish simulation {}",
+        simulate_cached_fish(INPUT, 256)
+    );
 }
---
+++ src/bin/day-7.rs
@@ -0,0 +1,54 @@
+use std::collections::HashMap;
+use std::fs;
+const INPUT: &str = "src/inputs/day-7.txt";
+
+fn read_input(input: &str) -> Vec<i32> {
+    let str_data = fs::read_to_string(input).expect("Unable to open input");
+    let vectorized = str_data
+        .split(",")
+        .map(|string| string.parse::<i32>().unwrap())
+        .collect();
+    vectorized
+}
+
+fn calculate_fuel_count(x: i32, crab_cache: &HashMap<i32, i32>, distanced: bool) -> i32 {
+    match distanced {
+        true=> {
+            crab_cache.iter().fold(0, |accum, (k,v)| {
+                let fuel_sum = (0..((k - x).abs() + 1)).fold(0, |accum, v| v+accum);
+                let value = accum + (v * fuel_sum);
+                value
+            })
+        },
+        false => {
+            crab_cache.iter().fold(0, |accum, (k,v)| {
+                let value = accum + (v * (k - x).abs());
+                value
+            })
+        }
+    }
+}
+
+fn move_crabs_brute_force(input: &str, distanced: bool) -> i32 {
+    let mut crabs: Vec<i32> = read_input(input);
+    crabs.sort();
+    let max = *crabs.last().unwrap() as usize;
+    let mut crabs_cache: HashMap<i32, i32> = HashMap::new();
+    crabs.iter().for_each(|c| {
+        crabs_cache.insert(
+            *c,
+            if crabs_cache.contains_key(c) {
+                crabs_cache[c] + 1
+            } else {
+                1
+            },
+        );
+    });
+    let distance_counters: Vec<i32> = (0..max + 1).map(|i| calculate_fuel_count(i as i32, &crabs_cache, distanced)).collect();
+    *distance_counters.iter().min().unwrap()
+}
+
+fn main() {
+    println!("Fuel cost {}", move_crabs_brute_force(INPUT, false));
+    println!("Crab-engineering fuel cost {}", move_crabs_brute_force(INPUT, true));
+}
